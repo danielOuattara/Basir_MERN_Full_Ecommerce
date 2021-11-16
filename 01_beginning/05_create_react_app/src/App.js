b04e7ddf6ab27763acf489cd961bdbd4539865ff
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-16 05:17:24 +0100

    05_starting_React_Part

---
+++ 01_beginning/05_create_react_app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { UseState } from './components/UseState';
+
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <UseState />
+    </div>
+  );
+}
+
+export default App;
