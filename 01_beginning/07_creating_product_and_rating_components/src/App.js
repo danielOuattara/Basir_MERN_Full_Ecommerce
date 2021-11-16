283b29ccb495f51a19b7fa4b817f46404b87409f
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-16 08:23:38 +0100

    06= github, 07 = product and rating components OK

---
+++ 01_beginning/07_creating_product_and_rating_components/src/App.js
@@ -0,0 +1,30 @@
+import data from './data';
+import Product from './components/Product';
+function App() {
+  return (
+     <div className="grid-container">
+        <header className="row">
+            <div>
+                <a  className="brand" href="/">amazona</a>
+            </div>
+            <div>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+              { data.products.map( product => {
+                  return (<Product key={product._id} product={product} />);
+              })}
+            </div>
+        </main>
+        <footer className="row center">
+            All right reserved
+        </footer>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ 01_beginning/07_creating_product_and_rating_components/src/components/Product.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Rating from './Rating';
+
+
+export default function Product(props) {
+    const { product }  = props;
+    return (
+            <div className="card">
+                <a href={`/product/${product._id}`}>
+                    {/* image size: 630 x 830  */}
+                    <img 
+                    className="medium" 
+                    src={product.image} 
+                    alt={product.name}/>
+                </a>
+                <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                        <h2>{product.name}</h2>
+                    </a>
+                    <Rating rating={product.rating} numReviews={product.numReviews}/>
+                    <div className="price">{product.price} €</div>
+                </div>
+            </div>
+    )
+}
